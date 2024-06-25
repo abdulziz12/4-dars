073b9e13b7e8ca3f2f82acd57173c893cc2c612a
abdulziz12 <abdurashidov200930@gmail.com>
2024-06-25 16:52:59 +0500

    dcs
    dfscsf

---
+++ script.js
@@ -0,0 +1,45 @@
+let random = Math.round(Math.random()*100)
+alert(`Random orqali chiqqan son: ${random}`)
+if (random > 10 && random < 30 ){
+    alert(`bu son fizz : ${random}`)
+}else{
+    alert(`bu son buzz : ${random}`)
+}
+var ikki = Math.round(Math.random()*100)
+alert(`Random orqali chiqqan son: ${ikki}`)
+if (ikki > 10 && ikki < 30 ){
+    alert(`bu son buzz : ${ikki}`)
+}else{
+    alert(`bu son fizz: ${ikki}`)
+}
+let random_2 = Math.round(Math.random()*100)
+alert(`Random orqali chiqqan son: ${random_2}`)
+if(random_2 > 70 && random_2 << 100  ){
+    alert(`bu son fizzbuzz :${random_2}`)
+}else{
+    alert(`bu son buzz :${random_2}`)
+}
+let boshqa = Math.round(Math.random()*100)
+alert(`Random orqali chiqqan son: ${boshqa}`)
+if (boshqa > 10 && boshqa < 30 ){
+    alert(`bu son fizz : ${boshqa}`)
+}else{
+    alert(`bu son buzz : ${boshqa}`)
+}
+
+var mars = Math.round(Math.random()*100)
+alert(`Random orqali chiqqan son: ${mars}`)
+if (mars > 10 && mars < 30 ){
+    alert(`bu son buzz : ${mars}`)
+}else{
+    alert(`bu son fizz: ${mars}`)
+}
+
+
+let tort = Math.round(Math.random()*100)
+alert(`Random orqali chiqqan son: ${tort}`)
+if(tort > 70 && tort << 100  ){
+    alert(`bu son fizzbuzz :${tort}`)
+}else{
+    alert(`bu son buzz :${tort}`)
+}
